93d349583efb38fce8d84308ee6b1c4ac1a494f7
RxPxMxN <raphat4961@gmail.com>
2023-01-24 13:00:26 +0700

    Lab05

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import LikeSad from "./components/LikeSad.vue";
+</script>
+<template>
+  <div class="row">
+    <div v-for="n in 6" :key="n" class="col-4">
+      <div class="card bg-dark" style="width: 18rem">
+        <img
+          src="./assets/genshinimpact_nilou_cover.jpg"
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body">
+          <p class="card-text">
+            Nilou
+          </p>
+        </div>
+      </div>
+      <LikeSad></LikeSad>
+    </div>
+  </div>
+</template>
